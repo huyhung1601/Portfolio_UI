cc90bc49d7782cd92f368612701d3a40066d8dee
Henry Nguyen <huyhung1601@gmail.com>
2021-04-23 08:45:08 +1000

    footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,23 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+import './Footer.css'
+import resumeData from '../../utils/resumeData'
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='footer'>
+            <div className='footer_left'>
+                <Typography className='footer_name'>
+                    {resumeData.name}
+                </Typography>
+            </div>
+            <div className='footer_right'>
+                <Typography className='footer_copyright'>
+                    Designed and Developed by <a href='/' target='_blank'>Huy Hung Nguyen</a>
+                    <br/>
+                    CLone idea from <a href="http://themeforest.net/user/tavonline" target='_blank'>Travonline</a>
+                </Typography>
+            </div>
         </div>
     )
 }
